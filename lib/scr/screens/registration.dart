af49bf3db520f9825f216fc2a5b846e72dcd9fc0
angebizzinfotech <ang.ebizzinfotech@gmail.com>
2021-03-17 14:14:16 +0530

    initial commit 0.1

---
+++ lib/scr/screens/registration.dart
@@ -0,0 +1,230 @@
+import 'package:flutter/material.dart';
+import 'package:food_course/scr/helpers/screen_navigation.dart';
+import 'package:food_course/scr/helpers/style.dart';
+import 'package:food_course/scr/providers/category.dart';
+import 'package:food_course/scr/providers/product.dart';
+import 'package:food_course/scr/providers/restaurant.dart';
+import 'package:food_course/scr/providers/user.dart';
+import 'package:food_course/scr/screens/login.dart';
+import 'package:food_course/scr/widgets/custom_text.dart';
+import 'package:food_course/scr/widgets/loading.dart';
+import 'package:provider/provider.dart';
+
+import 'home.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _key = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<UserProvider>(context);
+    final categoryProvider = Provider.of<CategoryProvider>(context);
+    final restaurantProvider = Provider.of<RestaurantProvider>(context);
+    final productProvider = Provider.of<ProductProvider>(context);
+
+    return Scaffold(
+      key: _key,
+      backgroundColor: white,
+      body: authProvider.status == Status.Authenticating? Loading() : SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 100,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset("images/logo.png", width: 100, height: 100,),
+              ],
+            ),
+
+            SizedBox(
+              height: 40,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: grey),
+                    borderRadius: BorderRadius.circular(15)
+                ),
+                child: Padding(padding: EdgeInsets.only(left: 10),
+                  child: TextFormField(
+                    controller: authProvider.name,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Username",
+                        icon: Icon(Icons.person)
+                    ),
+                    validator: (String value){
+                      if(value.isEmpty)
+                      {
+                        return 'Please Enter Username';
+                      }
+                      return null;
+                    },
+                  ),),
+              ),
+            ),
+
+
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: grey),
+                    borderRadius: BorderRadius.circular(15)
+                ),
+                child: Padding(padding: EdgeInsets.only(left: 10),
+                  child: TextFormField(
+                    controller: authProvider.email,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Email",
+                        icon: Icon(Icons.email)
+                    ),
+                    validator: (String value){
+                      if(value.isEmpty)
+                      {
+                        return 'Please  Enter Email';
+                      }
+                      if(!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)){
+                        return 'Please a valid Email';
+                      }
+                      return null;
+                    },
+                  ),),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: grey),
+                    borderRadius: BorderRadius.circular(15)
+                ),
+                child: Padding(padding: EdgeInsets.only(left: 10),
+                  child: TextFormField(
+                    controller: authProvider.password,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Password",
+                        icon: Icon(Icons.lock)
+                    ),
+                    validator: (String value){
+                      if(value.isEmpty )
+                      {
+                        return 'Please a Enter Password';
+                      }
+                      else if( value.length<6)
+                      {
+                        return 'enter at least 6 digit';
+                      }
+
+                      return null;
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: grey),
+                    borderRadius: BorderRadius.circular(15)
+                ),
+                child: Padding(padding: EdgeInsets.only(left: 10),
+                  child: TextFormField(
+                    controller: authProvider.cpassword,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: " Confirm Password",
+                        icon: Icon(Icons.lock)
+                    ),
+                    validator: (String value){
+                      if(value.isEmpty)
+                      {
+                        return 'Please re-enter password';
+                      }
+
+                      if(authProvider.password.text!=authProvider.cpassword.text){
+                        return "Password does not match";
+                      }
+                      return null;
+                    },
+
+                  ),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: GestureDetector(
+                onTap: ()async{
+                  print("BTN CLICKED!!!!");
+
+
+                  if(!await authProvider.signUp()){
+                    _key.currentState.showSnackBar(
+                        SnackBar(content: Text("Resgistration failed!"))
+                    );
+                    return;
+                  }
+                  categoryProvider.loadCategories();
+                 // restaurantProvider.loadSingleRestaurant();
+                  productProvider.loadProducts();
+                  authProvider.clearController();
+                  changeScreenReplacement(context, Home());
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: red,
+                      border: Border.all(color: grey),
+                      borderRadius: BorderRadius.circular(15)
+                  ),
+                  child: Padding(padding: EdgeInsets.only(top: 10, bottom: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CustomText(text: "Register", color: white, size: 22,)
+
+                      ],
+
+                    ),
+                  ),
+
+                ),
+              ),
+            ),
+
+            GestureDetector(
+              onTap: (){
+                changeScreen(context, LoginScreen());
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CustomText(text: "Already have account? plz login", size: 20,),
+                ],
+              ),
+            ),
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
